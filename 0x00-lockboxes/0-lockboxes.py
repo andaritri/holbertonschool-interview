34b4797e12fa3064aa4edba68f1fca334081d1e3
andaritri <andresat0712@gmail.com>
2021-12-09 23:17:59 -0500

    Third attemp

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,15 +4,17 @@
 
 def can_unlock_all(boxes):
     """Check if all of boxes can be opened"""
-    if not boxes[0] and len(boxes) > 1:
-        return False
-    if len(boxes) == 1:
-        return True
-    keys = {key for box in boxes for key in box}
-    for idx, _ in enumerate(boxes[1:], start=1):
+    num_boxes = len(boxes)
+    keys = set()
+    for idx, box in enumerate(boxes, start=1):
+        for key in box:
+            if key < num_boxes:
+                keys.add(key)
+                keys.update(boxes[key])
+            if len(keys) == num_boxes-1 or len(keys) == num_boxes:
+                return True
         if idx not in keys:
             return False
-    return True
 
 
 canUnlockAll = can_unlock_all
