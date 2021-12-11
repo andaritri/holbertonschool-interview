d521901e3fae0c40350fe6624a4780a526cd4e25
andaritri <andresat0712@gmail.com>
2021-12-11 15:50:41 -0500

    Fix checks

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,15 +4,17 @@
 
 def can_unlock_all(boxes):
     """Check if all of boxes can be opened"""
+    num_boxes = len(boxes)
     keys = set()
     for idx, box in enumerate(boxes, start=1):
         for key in box:
-            if key < len(boxes):
+            if key < num_boxes:
                 keys.add(key)
                 keys.update(boxes[key])
+            if len(keys) == num_boxes-1:
+                return True
         if idx not in keys:
             return False
-    return True
 
 
 canUnlockAll = can_unlock_all
